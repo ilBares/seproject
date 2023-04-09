ae848f5fb97ae0bcb4a3fe48f258fb7e452503dd
Marco Baresi <marco.baresi@intersail.it>
2023-04-09 18:03:44 +0200

    [Bevanda.java] implemented

---
+++ app/src/main/java/seproject/model/Bevanda.java
@@ -0,0 +1,19 @@
+package seproject.model;
+
+public class Bevanda {
+  private String nome;
+  private double quantita;
+
+  public Bevanda(String nome, double quantita) {
+    this.nome = nome;
+    this.quantita = quantita;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public double quantita() {
+    return quantita;
+  }
+}
