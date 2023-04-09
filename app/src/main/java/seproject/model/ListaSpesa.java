361f8dcae4468de328a2bb18497320d4c15c1118
chaimaelkoudri <c.elkoudri@studenti.unibs.it>
2023-04-09 23:36:44 +0200

    Basic constractors and getters implemented

--- app/src/main/java/seproject/model/ListaSpesa.java
+++ app/src/main/java/seproject/model/ListaSpesa.java
@@ -1,5 +1,25 @@
 package seproject.model;
 
 public class ListaSpesa {
-  
+  private String merce;
+  private double quantita;
+  private String unitaMisura;
+
+  public ListaSpesa(String merce, double quantita, String unitaMisura) {
+    this.merce = merce;
+    this.quantita = quantita;
+    this.unitaMisura = unitaMisura;
+  }
+
+  public String getMerce() {
+    return this.merce;
+  }
+
+  public double getQuantita() {
+    return this.quantita;
+  }
+
+  public String getUnitaMisura() {
+    return this.unitaMisura;
+  }
 }
--- app/src/main/java/seproject/model/MenuCarta.java
+++ app/src/main/java/seproject/model/MenuCarta.java
@@ -1,5 +1,15 @@
 package seproject.model;
 
+import java.util.List;
+
 public class MenuCarta {
-  
+    private List<Piatto> piatti;
+
+    public MenuCarta(List<Piatto> piatti) {
+        this.piatti = piatti;
+    }
+
+    public List<Piatto> getPiatti() {
+        return this.piatti;
+    }
 }
