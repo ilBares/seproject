716e37e71ea36107196c0ed7a669004be8095e10
Marco Baresi <marco.baresi@intersail.it>
2023-04-12 23:00:19 +0200

    Added type to JsonBevandeRepository

--- app/src/main/java/seproject/repository/JsonPiattiRepository.java
+++ app/src/main/java/seproject/repository/JsonPiattiRepository.java
@@ -1,12 +1,13 @@
 package seproject.repository;
 
 import java.util.List;
+import seproject.model.Piatto;
 import seproject.repository.interfaces.PiattiRepository;
 
-public class JsonPiattiRepository implements PiattiRepository {
+public class JsonPiattiRepository implements PiattiRepository<Piatto> {
 
     @Override
-    public List getPiatti() {
+    public List<Piatto> getPiatti() {
         // TODO Auto-generated method stub
         return null;
     }
